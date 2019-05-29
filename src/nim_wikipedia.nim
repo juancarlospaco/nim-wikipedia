c7c520738e0283a16e4737225483eb6be75a4971
Juan Carlos <juancarlospaco@gmail.com>
2019-05-29 00:55:19 -0300

    Init

--- src/nim_wikipedia.nim
+++ src/nim_wikipedia.nim
@@ -1,16 +1,17 @@
 import asyncdispatch, httpclient, strutils, json
 
 const
-  wikipediaUrl* = "https://wikipedia.org/w/api.php?format=json&formatversion=2" ## Wikipedia API URL.
-  mediaWikiUrl* = "https://mediawiki.org/w/api.php?format=json&formatversion=2" ## MediaWiki API URL.
-  wikipediaUrlTest* = "https://test.wikipedia.org/w/api.php?format=json&formatversion=2" ## Test Wikipedia API URL (Fake).
-  wikiMediaCommonsUrl* = "https://commons.wikimedia.org/w/api.php?format=json&formatversion=2" ## WikiMedia Commons API URL.
+  o = "?format=json&formatversion=2&limit=max&servedby=true&curtimestamp=true&responselanginfo=true&errorformat=plaintext"
+  wikipediaUrl* = "https://wikipedia.org/w/api.php" & o ## Wikipedia API URL.
+  mediaWikiUrl* = "https://mediawiki.org/w/api.php?format=json&formatversion=2&limit=max" & o ## MediaWiki API URL.
+  wikipediaUrlTest* = "https://test.wikipedia.org/w/api.php?format=json&formatversion=2&limit=max" & o ## Test Wikipedia API URL (Fake).
+  wikiMediaCommonsUrl* = "https://commons.wikimedia.org/w/api.php?format=json&formatversion=2&limit=max" & o ## WikiMedia Commons API URL.
 
 
 type
   WikipediaBase*[HttpType] = object ## Base object.
     timeout*: byte ## Timeout Seconds for API Calls, byte type, 0~255.
-    proxy*: Proxy  ## Network IPv4 / IPv6 Proxy support, Proxy type.
+    proxy*: Proxy             ## Network IPv4 / IPv6 Proxy support, Proxy type.
   Wikipedia* = WikipediaBase[HttpClient] ##  Sync Wikipedia API Client.
   AsyncWikipedia* = WikipediaBase[AsyncHttpClient] ## Async Wikipedia API Client.
 
